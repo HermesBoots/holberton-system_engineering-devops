2ff53929b88ff2648f632ec16afd07412323841d
HermesBoots <701@holbertonschool.com>
2019-09-16 23:48:34 +0000

    Replace urllib with requests in task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,19 +2,18 @@
 """Synthesize data from an HTTP API"""
 
 
-import json
+import requests
 import sys
-import urllib.request
 
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-    response = urllib.request.urlopen(url)
-    user = json.loads(response.read().decode())
+    response = requests.get(url)
+    user = response.json()
     response.close()
     url = 'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
-    response = urllib.request.urlopen(url)
-    tasks = json.loads(response.read().decode())
+    response = requests.get(url)
+    tasks = response.json()
     response.close()
     done = [task for task in tasks if task.get('completed')]
     print('Employee {} is done with tasks({}/{})'.format(
