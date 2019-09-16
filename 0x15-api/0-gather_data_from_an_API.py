309a0966a1373571ccf57d4b20760bcac96761e9
HermesBoots <701@holbertonschool.com>
2019-09-16 23:46:06 +0000

    Remove context managers from task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,16 +8,14 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    request = urllib.request.Request(
-        'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-    )
-    with urllib.request.urlopen(request) as response:
-        user = json.loads(response.read().decode())
-    request = urllib.request.Request(
-        'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
-    )
-    with urllib.request.urlopen(request) as response:
-        tasks = json.loads(response.read().decode())
+    url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    response = urllib.request.urlopen(url)
+    user = json.loads(response.read().decode())
+    response.close()
+    url = 'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
+    response = urllib.request.urlopen(url)
+    tasks = json.loads(response.read().decode())
+    response.close()
     done = [task for task in tasks if task.get('completed')]
     print('Employee {} is done with tasks({}/{})'.format(
         user.get('name'),
