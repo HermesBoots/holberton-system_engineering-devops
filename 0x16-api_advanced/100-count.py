141a624e2ffa266dbf22b629e889a0f7350efa6e
HermesBoots <701@holbertonschool.com>
2019-09-19 17:17:14 +0000

    Replace loops with generator expressions in task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -30,15 +30,17 @@ def count_words(subreddit, word_list, hot_list=[], after=None):
     hot_list.extend(p['data']['title'] for p in posts['data']['children'])
     after = posts['data']['after']
     if after is None:
-        words = collections.Counter(' '.join(hot_list).lower().split())
-        words = sorted(
-            ((word, words[word.lower()]) for word in word_list),
-            key=lambda pair: (-pair[1], pair[0])
+        counter = collections.Counter(
+            word
+            for title in hot_list for word in title.lower().split()
         )
-        for word, count in words:
-            if count > 0:
-                print(word, ': ', count, sep='')
-        if not any(count for _, count in words):
-            print()
+        print('\n'.join(
+            '{}: {}'.format(word, count)
+            for word, count in sorted((
+                (word, counter[word.lower()])
+                for word in word_list
+                if counter[word.lower()] > 0
+            ), key=lambda pair: (-pair[1], pair[0]))
+        ))
         return
     return count_words(subreddit, word_list, hot_list, after)
