b1807bfdeaeaa4323cad7d8bcf8c6544922dd07b
HermesBoots <701@holbertonschool.com>
2019-09-19 16:57:27 +0000

    Update task 3 to sort alphabetically and always print blank line

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,46 +3,42 @@
 
 
 import collections
-import http.client
 import io
 import json
+import requests
 import urllib.parse
 
 
-def count_words(subreddit, word_list, hot_list=[], after=None, client=None):
+def count_words(subreddit, word_list, hot_list=[], after=None):
     """Get frequency of keywords in posts in a Subreddit"""
 
-    path = '/r/' + urllib.parse.quote(subreddit, safe='') + '/hot.json'
+    path = 'https://www.reddit.com'
+    path += '/r/' + urllib.parse.quote(subreddit, safe='') + '/hot.json'
     path += '?raw_json=1'
     if after is not None:
         path += '&after=' + urllib.parse.quote_plus(after)
         path += '&count=' + str(len(hot_list))
-    if client is None:
-        client = http.client.HTTPSConnection('www.reddit.com')
-        client.connect()
-    client.putrequest('GET', path)
-    client.putheader('Connection', 'keep-alive')
-    client.putheader('User-Agent', 'python:hbtn701t3:0 (by /u/SamHermesBoots)')
-    client.endheaders()
-    response = client.getresponse()
-    if response.status != 200:
-        client.close()
-        return None
-    posts = json.load(io.TextIOWrapper(response, encoding='UTF-8'))
-    if response.getheader('Connection', 'close') == 'close':
-        client.close()
-        client = None
+    headers = {
+        'Connection': 'keep-alive',
+        'User-Agent': 'python:hbtn701t3:1 (by /u/SamHermesBoots)'
+    }
+    response = requests.get(path, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print()
+        return
+    posts = response.json()
     hot_list.extend(p['data']['title'] for p in posts['data']['children'])
     after = posts['data']['after']
     if after is None:
-        client.close()
         words = collections.Counter(' '.join(hot_list).lower().split())
         words = sorted(
             ((word, words[word.lower()]) for word in word_list),
-            key=lambda pair: pair[1],
-            reverse=True
+            key=lambda pair: (-pair[1], pair[0])
         )
         for word, count in words:
-            print(word, ': ', count, sep='')
+            if count > 0:
+                print(word, ': ', count, sep='')
+        if not any(count for _, count in words):
+            print()
         return
-    return count_words(subreddit, word_list, hot_list, after, client)
+    return count_words(subreddit, word_list, hot_list, after)
