cb09e79ad42ff2c1049bfd586b06022553f5c9b7
HermesBoots <701@holbertonschool.com>
2019-09-19 16:24:42 +0000

    Use requests modeul in task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,27 +2,24 @@
 """Get the number of subscribers to a Subreddit"""
 
 
-import http.client
 import io
 import json
+import requests
 import urllib.parse
 
 
 def number_of_subscribers(subreddit):
     """Get the number of subscribers to a Subreddit"""
 
-    path = '/r/' + urllib.parse.quote(subreddit, safe='') + '/about.json'
+    path = 'https://www.reddit.com'
+    path += '/r/' + urllib.parse.quote(subreddit, safe='') + '/about.json'
     path += '?raw_json=1'
-    client = http.client.HTTPSConnection('www.reddit.com')
-    client.connect()
-    client.putrequest('GET', path)
-    client.putheader('Connection', 'close')
-    client.putheader('User-Agent', 'python:hbtn701t0:1 (by /u/SamHermesBoots)')
-    client.endheaders()
-    response = client.getresponse()
-    if response.status != 200:
-        client.close()
+    headers = {
+        'Connection': 'close',
+        'User-Agent': 'python:hbtn701t0:2 (by /u/SamHermesBoots)'
+    }
+    response = requests.get(path, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    info = json.load(io.TextIOWrapper(response, encoding='UTF-8'))
-    client.close()
+    info = response.json()
     return info['data']['subscribers']
