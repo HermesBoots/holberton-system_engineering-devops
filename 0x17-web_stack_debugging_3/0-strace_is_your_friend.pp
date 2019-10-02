12e7f5b5b326363aa0c3ca8cff6f683d86b171bf
HermesBoots <701@holbertonschool.com>
2019-10-02 16:19:57 +0000

    Add comment to Puppet manifest

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# fix misspelled PHP extension in WordPress file
+
 exec { 'fix broken PHP extension':
   command => '/bin/sed -i \'s/\.phpp\>/\.php/\' wp-settings.php',
   cwd     => '/var/www/html'
