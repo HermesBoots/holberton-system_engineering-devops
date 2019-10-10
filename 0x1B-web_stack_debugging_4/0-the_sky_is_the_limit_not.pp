3083a9082e95ac78c9c90936ccad8833299595a8
HermesBoots <701@holbertonschool.com>
2019-10-10 15:04:28 +0000

    Add comment to task 0 file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,13 @@
+# fix a small open file limit enforced on Nginx
 file { 'let Nginx open more files':
   ensure  => file,
   path    => '/etc/default/nginx',
-  content => 'ULIMIT="-n 1048576"',
-  notify  => Service['nginx']
+  content => 'ULIMIT="-n 1048576"'
 }
 
 service { 'Nginx':
-  ensure => running,
-  name   => 'nginx',
-  enable => true
+  ensure     => running,
+  hasrestart => true,
+  name       => 'nginx',
+  subscribe  => File['/etc/default/nginx']
 }
